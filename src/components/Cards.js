8ed71df51cc5bd9df0199a3998554dd5e3ef0e4e
Govind8006 <govindaggarwal8006@gmail.com>
2023-03-26 19:17:38 +0530

    responsive

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -29,29 +29,31 @@ const Cards = () => {
       ) : (
         data.map((e, i) => {
           return (
-            <Link to={`/detail/${e.id}`}>
-              <div
-                key={i}
-                className="card font-medium shadow-lg p-2  hover:-translate-y-3 cursor-pointer mt-6 transition-all duration-500"
-              >
-                <img className="cards " src={e.image} alt={e.title} />
-                <h1>
-                  <span className="name text-gray-500">Name:</span> {e.title}
-                </h1>
-                <h1 className="flex items-center">
-                  <span className="text-gray-500 mr-1">Rating:</span>
-                  <ReactStars
-                    size={20}
-                    half={true}
-                    value={e.rating / e.rated}
-                    edit={false}
-                  />
-                </h1>
-                <h1>
-                  <span className="text-gray-500">Year:</span> {e.year}
-                </h1>
-              </div>
-            </Link>
+            <div className="small">
+              <Link to={`/detail/${e.id}`}>
+                <div
+                  key={i}
+                  className="card font-medium shadow-lg p-2  hover:-translate-y-3 cursor-pointer mt-6 transition-all duration-500"
+                >
+                  <img className="cards " src={e.image} alt={e.title} />
+                  <h1>
+                    <span className="name text-gray-500">Name:</span> {e.title}
+                  </h1>
+                  <h1 className="flex items-center">
+                    <span className="text-gray-500 mr-1">Rating:</span>
+                    <ReactStars
+                      size={20}
+                      half={true}
+                      value={e.rating / e.rated}
+                      edit={false}
+                    />
+                  </h1>
+                  <h1>
+                    <span className="text-gray-500">Year:</span> {e.year}
+                  </h1>
+                </div>
+              </Link>
+            </div>
           );
         })
       )}
